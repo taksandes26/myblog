f3783bc4c84b5c0b9427b9ef3ab176db1dc2cdec
Shubham Taksande <shubhamtaksande@Shubhams-MacBook-Air.local>
2024-05-07 17:10:40 +0530

    added models and migration to the app

--- posts/models.py
+++ posts/models.py
@@ -1,26 +1,22 @@
 from django.db import models
-
-
-class Auther(models.Model):
-    # id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    bio = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'DRAFT'),
+        ('published', 'PUBLISHED')
+    )
     # id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(to=Auther, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='draft')
+    publish = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
